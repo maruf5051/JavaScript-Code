21a761b7c36973a97a9e95e304313e35bf23ab2e
maruf5051 <marufsikder5051@gmail.com>
2025-05-23 21:10:18 +0600

    Add a file named dataType

---
+++ dataType.js
@@ -0,0 +1,27 @@
+// number = 2 to power 53
+number = 25
+console.log(number);
+
+// bigint
+bigint = 25154525455545544554454
+console.log(bigint);
+
+// string
+string = "Hello World"
+console.log(string);
+
+// boolean
+boolean = true
+console.log(boolean);
+
+// null => standalone value
+console.log(typeof null) // null is an object type in JavaScript
+
+// undefined => variable is declared but not assigned
+console.log(typeof undefined)
+
+// symbol => unique and immutable value
+// array => collection of values
+// function => callable object
+// date => object that represents a date and time
+// object => collection of key-value pairs
