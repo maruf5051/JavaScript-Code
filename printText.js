80c9a0b10120a99ee15bb063f0989d01fe92b517
maruf5051 <marufsikder5051@gmail.com>
2025-05-23 15:04:18 +0600

    Basic JavaScript Code

---
+++ printText.js
@@ -0,0 +1,9 @@
+console.log("Name: Maruf Sikder")
+console.log("Age: 22")
+console.log("Hobby: Playing Football")
+console.log("Profession: Student")
+console.log("Location: Dhaka, Bangladesh")
+console.log("Favorite Food: Biryani")
+console.log("Favorite Color: Black")
+console.log("Favorite Sport: Football")
+
