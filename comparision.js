bf1b614e30eb3dbe459fed6962b14147f3d4d2d9
maruf5051 <marufsikder5051@gmail.com>
2025-05-24 14:03:44 +0600

    comparision

--- comparision.js
+++ comparision.js
@@ -8,3 +8,27 @@ console.log(5 < 2)   // Less than
 console.log(6 <= 4)  // Less than or equal to
 console.log(6 >= 4)  // Greater than or equal to
 console.log(5 === 2) // Strict equality (both value and type)
+
+console.log("\n------------------------------\n")
+
+// Complare two different types of data
+console.log(5 == '5') // Loose equality (value only, different types)
+console.log(5 === '5') // Strict equality (value and type, different types)
+console.log("5" >= 2) // Loose comparison (string and number, string is converted to number)
+
+console.log("\n------------------------------\n")
+
+// identify some problems 
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log(NaN == 0); // NaN is not equal to anything, including itself
+
+console.log("------------------------------\n")
+
+console.log(undefined == 0); // undefined is not equal to anything, including null
+console.log(undefined > 0); // undefined is not greater than anything
+console.log(undefined >= 0); // undefined is not greater than or equal to anything
+
+
