192dca798b23d93176e6619f00249782a3374f07
maruf5051 <marufsikder5051@gmail.com>
2025-05-24 13:21:59 +0600

    add a new file named comparision

---
+++ comparision.js
@@ -0,0 +1,10 @@
+// Compare two integers numbers
+// console.log(2 = 2) // Syntax Error: Use '==' for comparison, '=' is for assignment
+console.log(2 == 2) // Correct comparison(loose equality)
+console.log(5 != 2) // Not equal to
+console.log(2 == 2) // Loose equality (value only)
+console.log(5 > 2)  // Greater than
+console.log(5 < 2)   // Less than
+console.log(6 <= 4)  // Less than or equal to
+console.log(6 >= 4)  // Greater than or equal to
+console.log(5 === 2) // Strict equality (both value and type)
